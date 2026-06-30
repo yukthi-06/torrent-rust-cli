388a243e57a3b0aa7697a57c7f0dfb1a2b42e4be
Shibu TN <tnshibu@yahoo.com>
2026-06-30 22:31:06 +0530

    compile error 1

--- crates/cli/build.rs
+++ crates/cli/build.rs
@@ -5,14 +5,14 @@ fn main() {
     println!("cargo:rerun-if-changed=../../.git/HEAD");
 
     let git_hash = Command::new("git")
-        .args(&["rev-parse", "HEAD"])
+        .args(["rev-parse", "HEAD"])
         .output()
         .ok()
         .and_then(|out| String::from_utf8(out.stdout).ok())
         .unwrap_or_else(|| "unknown".to_string());
 
     let git_date = Command::new("git")
-        .args(&["log", "-1", "--format=%ci"])
+        .args(["log", "-1", "--format=%ci"])
         .output()
         .ok()
         .and_then(|out| String::from_utf8(out.stdout).ok())
